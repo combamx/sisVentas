c3111a928a4c797b290e90248897004f90899e72
Omar Cortes <omar.cortes.casillas@gmail.com>
2021-04-02 21:11:35 -0600

    cambios

--- Models/VentaRealContext.cs
+++ Models/VentaRealContext.cs
@@ -31,8 +31,7 @@ namespace WSVentas.Models
             if (!optionsBuilder.IsConfigured)
             {
             //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                //optionsBuilder.UseSqlServer("Data Source=DESKTOP-D2UV6S5;Initial Catalog=VentaReal;Integrated Security=True");
-                optionsBuilder.UseSqlServer(Configuration.GetConnectionString("BloggingDatabase"));
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-D2UV6S5;Initial Catalog=VentaReal;Integrated Security=True");
             }
         }
 
